118d9af1252be7672bb0206f5b56d6ccdead10bb
Jurian <j.baas@uu.nl>
2019-02-22 17:18:20 +0100

    Fixed bug where formula was not constructed

--- src/combine-ecartico-onstage.R
+++ src/combine-ecartico-onstage.R
@@ -30,6 +30,7 @@ linkset$ecartico <- sapply(linkset$ecartico, function(x){
 
 combined.data <- data.frame(onstage = onstage.vectors[linkset$onstage], ecartico = ecartico.vectors[linkset$ecartico])
 
+n <- names(combined.data)
 f <- as.formula(paste(paste(n[1:50], collapse = " + ") , "~", paste(n[51:100], collapse = " + ")))
 nn <- neuralnet(f, data=combined.data, hidden=c(100,200,100), linear.output=T)
 pr.nn <- compute(nn,combined.data[,1:50])
