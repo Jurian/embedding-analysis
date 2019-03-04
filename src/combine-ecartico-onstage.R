ed37b6b538ad25dbe7a66cdb5d2f27b90f55d389
Jurian <j.baas@uu.nl>
2019-03-04 14:51:03 +0100

    Changed neural net to not use as many nodes

--- src/combine-ecartico-onstage.R
+++ src/combine-ecartico-onstage.R
@@ -30,7 +30,7 @@ combined.data <- data.frame(onstage = onstage.vectors[linkset$onstage], ecartico
 
 n <- names(combined.data)
 f <- as.formula(paste(paste(n[1:50], collapse = " + ") , "~", paste(n[51:100], collapse = " + ")))
-nn <- neuralnet(f, data=combined.data, hidden=c(100,200,100), linear.output=T)
+nn <- neuralnet(f, data=combined.data, hidden=c(50,100,50), linear.output=T)
 pr.nn <- compute(nn,combined.data[,1:50])
 
 rm(f,n)
